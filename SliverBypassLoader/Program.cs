b55570dcdb8730a9167aa22a87a3814b4a950000
jamu85 <jamu85@icloud.com>
2024-09-25 16:41:49 +0700

    Add a Project for a bypass stager

---
+++ SliverBypassLoader/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using System.Collections.Generic;
+using System.Configuration.Install;
+
+namespace SliverBypassLoader
+{
+    class altbypass
+    { 
+    
+    }
+
+    // InstallUtill uninstall bypass
+    [System.ComponentModel.RunInstaller(true)]
+    public class Loader : System.Configuration.Install.Installer
+    {
+
+    }
+}
